28d14506aa397ff863d2d31f9cacbeba86d4f8a4
Sankalp Khanna <khanna.1058@gmail.com>
2025-06-22 10:11:05 +0800

    fix: load default prompt file on build

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -2,9 +2,18 @@ import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: 'DEFAULT_PROMPT.md',
+        to: 'DEFAULT_PROMPT.md'
+      }
+    ]
+  })
 ];
