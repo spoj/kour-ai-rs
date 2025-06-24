5096f9845fabc53aeff11527aa3e2494b50b271a
spoj <yuyeung.wong@gmail.com>
2025-06-24 13:38:50 +0800

    updates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,23 @@
+use serde_json::from_value;
+use tauri::{AppHandle, Result};
+use tauri_plugin_store::StoreBuilder;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn increment_and_show(app: &AppHandle) -> Result<usize> {
+    let builder = StoreBuilder::new(app, "store.bin");
+    let store = builder.build().expect("store not found");
+    let counter: usize = store
+        .get("counter")
+        .and_then(|v| from_value(v).ok())
+        .unwrap();
+    let counter = counter + 1;
+    store.set("counter", counter);
+    Ok(counter)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,6 +26,11 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let builder = StoreBuilder::new(app, "store.bin");
+            let _store = builder.build();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
