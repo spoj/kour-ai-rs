33b3c0ae61735e77e0b0bf5f5ecf0ebd998f0f19
spoj <yuyeung.wong@gmail.com>
2025-07-19 01:10:24 +0800

    fix: make list_files recursively list all files

--- src-tauri/src/fd.rs
+++ src-tauri/src/fd.rs
@@ -4,7 +4,7 @@ use crate::settings::get_root;
 
 pub fn list_files() -> Result<Vec<String>, crate::Error> {
     let root = get_root()?;
-    let res = glob::glob(&root).map_err(|_| anyhow!("glob error"))?;
+    let res = glob::glob(&format!("{root}/**/*")).map_err(|_| anyhow!("glob error"))?;
     Ok(res
         .flat_map(|p| match p {
             Ok(p) => Some(p.to_string_lossy().into_owned()),
