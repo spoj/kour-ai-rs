10806475891f63ea9163f905b13a1d6b8e87c224
spoj <yuyeung.wong@gmail.com>
2025-07-19 02:41:15 +0800

    refactor: use ignore::Walk instead of walkdir

--- src-tauri/src/tools/extract/extract_zip.rs
+++ src-tauri/src/tools/extract/extract_zip.rs
@@ -1,6 +1,5 @@
 use std::fs as std_fs;
 use std::path::Path;
-use walkdir::WalkDir;
 
 use crate::Result;
 use crate::error::Error;
@@ -13,9 +12,10 @@ pub fn extract_zip(file_path: &Path, output_dir: &Path) -> Result<Vec<String>> {
         .map_err(|_| Error::Tool("cannot extract".to_string()))?;
 
     let mut extracted_files = Vec::new();
-    for entry in WalkDir::new(output_dir) {
+    for entry in ignore::Walk::new(output_dir) {
         if let Ok(entry) = entry
-            && entry.file_type().is_file()
+            && let Some(file_type) = entry.file_type()
+            && file_type.is_file()
             && let Some(path_str) = entry.path().to_str()
         {
             extracted_files.push(path_str.to_string());
