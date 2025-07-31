5ed2db2427a5569bbdf117ce0087667bf536ba5d
Matthew Wong <yuyeung.wong@gmail.com>
2025-07-31 20:35:28 +0800

    refactor[search]: turn find_by_globs into a free function

--- src-tauri/src/search.rs
+++ src-tauri/src/search.rs
@@ -145,7 +145,7 @@ impl SearchState {
 
         let globs = globs.to_string();
         let files = self.full_list.read().unwrap();
-        let mut res = Self::find_by_globs(&files, &globs);
+        let mut res = find_by_globs(&files, &globs);
         if let Ok(ref mut v) = res {
             *self.last_search.write().unwrap() = globs.to_string();
             *self.last_search_result.write().unwrap() = v.iter().cloned().collect();
@@ -158,43 +158,6 @@ impl SearchState {
             }
         }
         res
-    }
-
-    fn find_by_globs(paths: &HashSet<String>, globs: &str) -> Result<Vec<String>, crate::Error> {
-        let lex = Shlex::new(globs);
-        let mut no_set = GlobSetBuilder::new();
-        let mut yes_set = GlobSetBuilder::new();
-        for pat in lex {
-            if let Some(pat) = pat.strip_prefix('!') {
-                let glob = GlobBuilder::new(&format!("*{pat}*"))
-                    .case_insensitive(true)
-                    .backslash_escape(false)
-                    .literal_separator(false)
-                    .build()?;
-                no_set.add(glob);
-            } else {
-                let glob = GlobBuilder::new(&format!("*{pat}*"))
-                    .case_insensitive(true)
-                    .backslash_escape(false)
-                    .literal_separator(false)
-                    .build()?;
-                yes_set.add(glob);
-            }
-        }
-        let yes_set = yes_set.build()?;
-        let no_set = no_set.build()?;
-
-        let out: Vec<_> = paths
-            .par_iter()
-            .flat_map(|path| {
-                if yes_set.matches(path).len() == yes_set.len() && no_set.matches(path).is_empty() {
-                    Some(path.to_string())
-                } else {
-                    None
-                }
-            })
-            .collect();
-        Ok(out)
     }
 }
 
@@ -258,6 +221,21 @@ where
     }
 }
 
+fn find_by_globs(paths: &HashSet<String>, globs: &str) -> Result<Vec<String>, crate::Error> {
+    let (yes_set, no_set) = dual_globsets(globs)?;
+    let out: Vec<_> = paths
+        .par_iter()
+        .flat_map(|path| {
+            if yes_set.matches(path).len() == yes_set.len() && no_set.matches(path).is_empty() {
+                Some(path.to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+    Ok(out)
+}
+
 pub static SEARCH_STATE: LazyLock<SearchState> = LazyLock::new(Default::default);
 pub static SELECTION_STATE: LazyLock<SelectionState> = LazyLock::new(Default::default);
 
@@ -291,7 +269,7 @@ pub fn selection_clear() {
 mod tests {
     use std::collections::HashSet;
 
-    use super::SearchState;
+    use super::find_by_globs;
     use shlex::Shlex;
 
     fn shlex_split(input: &str) -> Vec<String> {
@@ -314,43 +292,13 @@ mod tests {
         let paths: HashSet<String> =
             HashSet::from([r#"local work\savv2\something.xlsx"#.to_string()]);
 
-        assert_eq!(
-            SearchState::find_by_globs(&paths, "savv something")
-                .unwrap()
-                .len(),
-            1
-        );
-        assert_eq!(
-            SearchState::find_by_globs(&paths, "something savv")
-                .unwrap()
-                .len(),
-            1
-        );
-        assert_eq!(
-            SearchState::find_by_globs(&paths, "something")
-                .unwrap()
-                .len(),
-            1
-        );
-        assert_eq!(
-            SearchState::find_by_globs(&paths, "something not")
-                .unwrap()
-                .len(),
-            0
-        );
-        assert_eq!(
-            SearchState::find_by_globs(&paths, "something !savv")
-                .unwrap()
-                .len(),
-            0
-        );
-        assert_eq!(SearchState::find_by_globs(&paths, "").unwrap().len(), 1);
-        assert_eq!(SearchState::find_by_globs(&paths, "work").unwrap().len(), 1);
-        assert_eq!(
-            SearchState::find_by_globs(&paths, r"work\\savv")
-                .unwrap()
-                .len(),
-            1
-        );
+        assert_eq!(find_by_globs(&paths, "savv something").unwrap().len(), 1);
+        assert_eq!(find_by_globs(&paths, "something savv").unwrap().len(), 1);
+        assert_eq!(find_by_globs(&paths, "something").unwrap().len(), 1);
+        assert_eq!(find_by_globs(&paths, "something not").unwrap().len(), 0);
+        assert_eq!(find_by_globs(&paths, "something !savv").unwrap().len(), 0);
+        assert_eq!(find_by_globs(&paths, "").unwrap().len(), 1);
+        assert_eq!(find_by_globs(&paths, "work").unwrap().len(), 1);
+        assert_eq!(find_by_globs(&paths, r"work\\savv").unwrap().len(), 1);
     }
 }
